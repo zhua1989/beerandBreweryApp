a5a4c4c29c10228eab1dc9e5e0dde25952b846df
Anthony Zhu <zhua89@gmail.com>
2015-08-28 16:15:03 -0400

    added b crypt

--- awesome_app/app/controllers/sessions_controller.rb
+++ awesome_app/app/controllers/sessions_controller.rb
@@ -7,9 +7,19 @@ class SessionsController < ApplicationController
 
 
 
+
   def create
+    user = User.find_by({name: params[:name]})
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to sessions_new_path
+    end
 
-
+    #logged in means a user_id is stored in a session
   end
+
+  
 
 end
--- awesome_app/app/models/user.rb
+++ awesome_app/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
+
   has_many :tastings
   has_many :comments
   has_many :beers, through: :tastings
