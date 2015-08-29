5c48dc9b8bfec72b96b2c576528b7c919ef7fb97
Anthony Zhu <zhua89@gmail.com>
2015-08-29 13:48:58 -0400

    user methods

--- awesome_app/app/controllers/users_controller.rb
+++ awesome_app/app/controllers/users_controller.rb
@@ -1,12 +1,34 @@
 class UsersController < ApplicationController
 
+    
   def new
-
+    @user = User.new
   end
 
 
   def create
+    new_user = User.create(user_params)
+    redirect_to root_path
+  end
 
+
+  def show
+    actual_user = User.find(session[:user_id])
+      if logged_in? && check_current_user? #this is our definition of logged in
+        @current_user = User.find(session[:user_id])
+        @beers = @current_user.beers
+      else
+        redirect_to user_path(actual_user)
+      end
+    end
+  end
+
+
+  private
+
+
+  def user_params
+    params.require(:user).permit(:name, :password, :image_url)
   end
 
 
