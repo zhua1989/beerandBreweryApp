94267c89abb1c8b77af4d4209d5ed05c98b6d5ec
Brian Johnson <johnba4@gmail.com>
2015-08-30 10:54:42 -0400

    completed the beers controller and added the api key to envrionment variables file in the config folder and changed config/application.rb to look in that folder

--- awesome_app/app/controllers/beers_controller.rb
+++ awesome_app/app/controllers/beers_controller.rb
@@ -1,23 +1,66 @@
+require 'httparty'
+
 class BeersController < ActivationController
   def search # receives GET request from user#show looking for beers and renders the search results page
-    f = File.read("../../../secrets.json")
-    data = JSON.parse(f)
-    key = data["key"]
+# read API key from config/local_env.yml
+    apiKey = ENV["BEER_API_KEY"]
 
-    userQuery = params[:search]
-    APIurl = "http://api.brewerydb.com/v2/search/?key=" + apiKey + "&q=" + userQuery+ "&type=beer&withBreweries=y"
-    # hit the API for search results
+# build the query string
+    userQuery = params[:search].chomp
+    APIurl = "http://api.brewerydb.com/v2/search/?key=" + apiKey + "&q='" + userQuery + "'&type=beer&withBreweries=y"
 
-    # further filter the API results 
+# hit the API for search results
+    request = HTTParty.get(APIurl)
 
-    # pass the restults to the search view
-  end
+# further filter the API results based on the filter param in the search page
+    filter = params[:filter]
+    beersUserTasted = User.find_by[session[:user_id]].tastings.pluck(beer_api_id)
+
+    if filter == "topResults"
+      @beers = request.data.take(15)
+      @alreadyTasted = @beers.map { |beer| beersUserTasted.include?(beer.id) }
+    elsif filter == "onlyName"
+      # ternary operatory returns true if the the userQuery is found in the beer.name
+      matches = request.data.select { |beer| beer.name.chomp.downcase.match(userQuery.downcase) ? true : false}
+      @beers = matches.take(15)
+      @alreadyTasted = @beers.map { |beer| beersUserTasted.include?(beer.id) }
+    elsif filter == "onlyDescription"
+      # ternary operatory returns true if the the userQuery is found in the beer.description
+      matches = request.data.select { |beer| beer.description.chomp.downcase.match(userQuery.downcase) ? true : false}
+      @beers = matches.take(15)
+      @alreadyTasted = @beers.map { |beer| beersUserTasted.include?(beer.id) }
+    end # end if statement
+  
+  end # end search method
 
   def create
-    # receives the POST request from search results page and adds the beer to the database
+# grabbing form data based on the value of the radio button that was selected
+    id = params[beer_id]
+    name = params["#{id}_name"]
+    description = params["#{id}_description"]
+    abv = params["#{id}_abv"]
+    image_url = params["#{id}_image_url"]
+    brewery_name = params["#{id}_brewery_name"]
+
+    if !Beer.find_by(api_id: id) && !Tasting.where({user_id: session[:user_id], beer_api_id: id})
+      newBeer = Beer.create({api_id: id, name: name, description: description, abv: abv, image_url: image_url, brewery_name: brewery_name})
+      Tasting.create({user_id: session[:user_id], beer_id: newBeer.id})
+    
+    elsif Beer.find_by(api_id: id) && !Tasting.where({user_id: session[:user_id], beer_api_id: id})
+      newBeer = Beer.find_by(api_id: id)
+      Tasting.create({user_id: session[:user_id], beer_id: newBeer.id})
+    
+    else 
+      redirect_to users_path()
+    end
+
+# redirect to the user's show page after adding a beer to their collection
+    redirect_to users_path(session[:user_id])
   end
 
   def show
+    @beer = Beer.find(params[:id])
+    @comments = @beer.comments
   end
 
   def destroy
