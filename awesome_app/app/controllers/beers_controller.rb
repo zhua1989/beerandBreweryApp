19850c06a858fa8a05a76498ff40366be3603d57
Anthony Zhu <zhua89@gmail.com>
2015-08-31 13:22:15 -0400

    resolved

--- awesome_app/app/controllers/beers_controller.rb
+++ awesome_app/app/controllers/beers_controller.rb
@@ -1,6 +1,11 @@
 require 'httparty'
 
 class BeersController < ApplicationController
+<<<<<<< HEAD
+=======
+
+  
+>>>>>>> sprockets
   def search # receives GET request from user#show looking for beers and renders the search results page
 # read API key from config/local_env.yml
     apiKey = ENV["BEER_API_KEY"]
