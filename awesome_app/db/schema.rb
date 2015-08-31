dea2d9056fec39b7d69d08f202aa42d69fa7cad7
Brian Johnson <johnba4@gmail.com>
2015-08-31 14:41:17 -0400

    beer controller in a finished state

--- awesome_app/db/schema.rb
+++ awesome_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831134617) do
+ActiveRecord::Schema.define(version: 20150831172556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,12 @@ ActiveRecord::Schema.define(version: 20150831134617) do
   create_table "beers", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "beer_api_id"
+    t.string   "abv"
+    t.string   "image_url"
+    t.string   "brewery_name"
   end
 
   create_table "comments", force: :cascade do |t|
