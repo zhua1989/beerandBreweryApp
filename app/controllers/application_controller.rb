954476df4f57e3670717c4b056d106ea4a6fcf41
ashley baird <ashleybaird99@gmail.com>
2015-09-01 12:09:19 -0400

    fixed errors in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
+
 
 
     def logged_in?
@@ -26,6 +26,5 @@ class ApplicationController < ActionController::Base
     end
   
 
-=======
->>>>>>> b8b7660dbf352ad5ab7028027078c4ffc44dbd87
+
 end
