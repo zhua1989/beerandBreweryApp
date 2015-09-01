daf3d31cb4fb6c57ac69922c9fcba66d0dc4820c
wightman <davidwightmanmusic@gmail.com>
2015-09-01 09:36:58 -0400

    form_tag in beers#show

--- awesome_app/app/controllers/comments_controller.rb
+++ awesome_app/app/controllers/comments_controller.rb
@@ -1,15 +1,19 @@
 class CommentsController < ApplicationController
 
 	def create
-		if (session[:logged_in])
-			Comment.create({user_id: session[:user_id], beer_id: params[:id], content: params[:content]})
-			redirect_to beer_path
+		if (session[:user_id])
+			Comment.create({user_id: session[:user_id], beer_id: comment_params[:beer_id], content: comment_params[:content]})
+			redirect_to beer_path(comment_params[:beer_id])
+		else
+			redirect_to beer_path(comment_params[:beer_id])
 		end
 
-		private
-
-		def comments_params
-			params.require(:comment).permit(:user_id, :beer_id, :content)
-		end
 	end
+
+private
+
+	def comment_params
+		params.permit(:beer_id, :content)
+	end
+
 end
